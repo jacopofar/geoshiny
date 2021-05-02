73701c43489f7f40a2cff2bafda3d9d94d625f9e
Jacopo Farina <jacopo.farina@flixbus.com>
2021-05-02 14:41:30 +0200

    Publish to pypi

--- setup.py
+++ setup.py
@@ -4,12 +4,16 @@ from setuptools import setup, find_packages
 setup(
     name="geoshiny",
     version="0.0.1",
-    description="GIS data rendered",
+    description="GIS data renderer",
+    url="https://github.com/jacopofar/geoshiny",
     long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
     author="Jacopo Farina",
+    author_email="jacopo1.farina@gmail.com",
     license="MIT",
     python_requires=">=3.8",
     packages=find_packages(),
+    include_package_data=False,
     install_requires=[
         # display, and quick troubleshooting
         "matplotlib"
